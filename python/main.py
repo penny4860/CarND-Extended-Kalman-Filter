01cca72a55566c06a816a668df7b4f40fa6304a6
jeongjoonsup <penny4860@gmail.com>
2017-11-02 22:27:33 +0900

    to port comment added

--- python/main.py
+++ python/main.py
@@ -10,6 +10,9 @@ lines = f.readlines()
 estimations = []
 ground_truth = []
 
+# Create a Kalman Filter instance
+# fusionEKF = FusionEKF()
+
 for line in lines:
     line = str(line).split()
     sensor = line[0]
@@ -37,6 +40,34 @@ for line in lines:
     
     gt_values = np.array([x_gt, y_gt, vx_gt, vy_gt])
     ground_truth.append(gt_values)
+    
+    
+#     # Call ProcessMeasurment(meas_package) for Kalman filter
+#     fusionEKF.ProcessMeasurement(meas_package);
+# 
+#     # Push the current estimated x,y positon from the Kalman filter's state vector
+#     VectorXd estimate(4);
+# 
+#     double p_x = fusionEKF.ekf_.x_(0);
+#     double p_y = fusionEKF.ekf_.x_(1);
+#     double v1  = fusionEKF.ekf_.x_(2);
+#     double v2 = fusionEKF.ekf_.x_(3);
+# 
+#     estimate(0) = p_x;
+#     estimate(1) = p_y;
+#     estimate(2) = v1;
+#     estimate(3) = v2;
+# 
+#     estimations.push_back(estimate);
+# 
+#     VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+# 
+#     cout << count << "    RMSE:    "<< RMSE(0) << ", " << RMSE(1) << ", " << RMSE(2) << ", " << RMSE(3) << "\n";
+#     # //        498    RMSE:    0.0973178, 0.0854597, 0.451267, 0.439935
+#     # //        499    RMSE:    0.0972256, 0.0853761, 0.450855, 0.439588
+#     count++;
+
+    
     
 print(ground_truth)
     
