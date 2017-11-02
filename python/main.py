93ba62ff4d83a4b2df5af50e964061e79d139056
jeongjoonsup <penny4860@gmail.com>
2017-11-02 22:23:55 +0900

    ground_truth.push_back(gt_values); porting

--- python/main.py
+++ python/main.py
@@ -7,6 +7,9 @@ filename = "..//data//obj_pose-laser-radar-synthetic-input.txt"
 f = open(filename, 'r')
 lines = f.readlines()
 
+estimations = []
+ground_truth = []
+
 for line in lines:
     line = str(line).split()
     sensor = line[0]
@@ -14,6 +17,8 @@ for line in lines:
     meas_package = MeasurementPackage()
     if sensor == "R":
         ro, theta, ro_dot, timestamp = line[1:5]
+        x_gt, y_gt, vx_gt, vy_gt = line[5:9]
+        
         print(ro, theta, ro_dot)
         
         meas_package.sensor_type_ = "R";
@@ -22,10 +27,16 @@ for line in lines:
 
     else:
         px, py, timestamp = line[1:4]
+        x_gt, y_gt, vx_gt, vy_gt = line[4:8]
+
         print(px, py)
         
         meas_package.sensor_type_ = "L";
         meas_package.raw_measurements_ = np.array([px, py]).reshape(-1,1)
         meas_package.timestamp_ = timestamp;
-        
+    
+    gt_values = np.array([x_gt, y_gt, vx_gt, vy_gt])
+    ground_truth.append(gt_values)
+    
+print(ground_truth)
     
