a567079fb493f36e2ab603835b83e175564aff1d
jeongjoonsup <penny4860@gmail.com>
2017-11-02 22:12:07 +0900

    MeasurementPackage implemented

---
+++ python/pkg/measurement_package.py
@@ -0,0 +1,15 @@
+
+
+class MeasurementPackage(object):
+    def __init__(self):
+        # int
+        self.timestamp_ = None
+
+        # Radar / Laser        
+        self.sensor_type_ = None
+        
+        # array
+        self.raw_measurements_ = None
+
+
+
