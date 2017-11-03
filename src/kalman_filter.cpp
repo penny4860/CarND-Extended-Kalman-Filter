515797ac95c4f59735247a2aae70379623edc3da
jeongjoonsup <penny4860@gmail.com>
2017-11-03 21:15:15 +0900

    nit

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -21,64 +21,51 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-  x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+	// predict state & its covariance
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+	VectorXd z_pred = H_ * x_;
 
-  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
 
-  VectorXd y = z - z_pred;
-  MatrixXd Ht = H_.transpose();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd S = H_ * PHt + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K = PHt * Si;
-
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
-
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 VectorXd RadarCartesianToPolar(const VectorXd &x_state){
-  /*
-   * convert radar measurements from cartesian coordinates (x, y, vx, vy) to
-   * polar (rho, phi, rho_dot) coordinates
-  */
-  float px, py, vx, vy;
-  px = x_state[0];
-  py = x_state[1];
-  vx = x_state[2];
-  vy = x_state[3];
+	// (px, py, vx, vy) => (rho, phi, rho_dot)
+	const float EPSILON	= 0.000001;
+	float px, py, vx, vy;
+	px = x_state[0];
+	py = x_state[1];
+	vx = x_state[2];
+	vy = x_state[3];
 
-  float rho, phi, rho_dot;
-  rho = sqrt(px*px + py*py);
-  phi = atan2(py, px);  // returns values between -pi and pi
+	float rho, phi, rho_dot;
+	rho = sqrt(px*px + py*py);
+	phi = atan2(py, px);  // returns values between -pi and pi
 
-  // if rho is very small, set it to 0.0001 to avoid division by 0 in computing rho_dot
-  if(rho < 0.000001)
-    rho = 0.000001;
+	if(rho < EPSILON)
+		rho = EPSILON;
 
-  rho_dot = (px * vx + py * vy) / rho;
+	rho_dot = (px * vx + py * vy) / rho;
 
-  VectorXd z_pred = VectorXd(3);
-  z_pred << rho, phi, rho_dot;
+	VectorXd z_pred = VectorXd(3);
+	z_pred << rho, phi, rho_dot;
 
-  return z_pred;
-
+	return z_pred;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
