c30cddb14a05966840154b2ceff7db6919f635aa
jeongjoonsup <penny4860@gmail.com>
2017-11-03 21:22:42 +0900

    code cleaning

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -4,6 +4,9 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
+static VectorXd _cartesian_to_polar(const VectorXd &x_state);
+static VectorXd _innovate_polar(const VectorXd &z, const VectorXd &z_pred);
+
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
@@ -42,7 +45,33 @@ void KalmanFilter::Update(const VectorXd &z) {
 	P_ = (I - K * H_) * P_;
 }
 
-VectorXd RadarCartesianToPolar(const VectorXd &x_state){
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+    * update the state by using Extended Kalman Filter equations
+  */
+
+  // convert radar measurements from cartesian coordinates (x, y, vx, vy) to polar (rho, phi, rho_dot).
+  VectorXd z_pred = _cartesian_to_polar(x_);
+  VectorXd y = _innovate_polar(z, z_pred);
+
+  // following is exact the same as in the function of KalmanFilter::Update()
+  MatrixXd Ht = H_.transpose();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+
+static VectorXd _cartesian_to_polar(const VectorXd &x_state)
+{
 	// (px, py, vx, vy) => (rho, phi, rho_dot)
 	const float EPSILON	= 0.000001;
 	float px, py, vx, vy;
@@ -66,8 +95,7 @@ VectorXd RadarCartesianToPolar(const VectorXd &x_state){
 	return z_pred;
 }
 
-
-VectorXd InnovateRadarMeas(const VectorXd &z, const VectorXd &z_pred)
+static VectorXd _innovate_polar(const VectorXd &z, const VectorXd &z_pred)
 {
 	  VectorXd y = z - z_pred;
 
@@ -82,26 +110,3 @@ VectorXd InnovateRadarMeas(const VectorXd &z, const VectorXd &z_pred)
 	  return y;
 }
 
-
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-    * update the state by using Extended Kalman Filter equations
-  */
-
-  // convert radar measurements from cartesian coordinates (x, y, vx, vy) to polar (rho, phi, rho_dot).
-  VectorXd z_pred = RadarCartesianToPolar(x_);
-  VectorXd y = InnovateRadarMeas(z, z_pred);
-
-  // following is exact the same as in the function of KalmanFilter::Update()
-  MatrixXd Ht = H_.transpose();
-  MatrixXd PHt = P_ * Ht;
-  MatrixXd S = H_ * PHt + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K = PHt * Si;
-
-  //new estimate
-  x_ = x_ + (K * y);
-  long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
-}
